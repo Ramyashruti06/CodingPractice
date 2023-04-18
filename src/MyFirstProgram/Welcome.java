9a2ab9a4d569925910e278945c13332a6307977f
CodeTalks <info@codetalksdna.com>
2023-04-18 10:41:20 +0530

    Removed Unwanted Class and added new

--- src/MyFirstProgram/Welcome.java
+++
@@ -1,7 +0,0 @@
-package MyFirstProgram;
-
-public class Welcome {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java.");
-    }
-}
---
+++ src/codetalksdna/basicPrograms/PrintWelcomeToJava.java
@@ -0,0 +1,7 @@
+package codetalksdna.basicPrograms;
+
+public class PrintWelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!!");
+    }
+}
