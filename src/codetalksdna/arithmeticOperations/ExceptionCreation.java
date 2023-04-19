f00ecdae9f0e1427d26ecce297e726025e99b64e
CodeTalks <info@codetalksdna.com>
2023-04-19 11:16:00 +0530

    Added String class.

---
+++ src/codetalksdna/arithmeticOperations/ExceptionCreation.java
@@ -0,0 +1,14 @@
+package codetalksdna.arithmeticOperations;
+
+public class ExceptionCreation {
+    public static void main(String[] args) {
+        int number = 10;
+        int result = 0;
+        try {
+            result = number / 0;
+            System.out.println("Result in try Block" + " " + result);
+        } catch (Exception e) {
+            System.out.println("Result in Catch Block" + " " + result);
+        }
+    }
+}
--- src/codetalksdna/arithmeticOperations/MultiplicationOfNumbers.java
+++ src/codetalksdna/arithmeticOperations/MultiplicationOfNumbers.java
@@ -2,9 +2,17 @@ package codetalksdna.arithmeticOperations;
 
 public class MultiplicationOfNumbers {
     public static void main(String[] args) {
+        Calculation calculation = new Calculation();
+        int mul = calculation.multiplyingDigits();
+        System.out.println("Multiplication of Two Number is::" + " " + mul);
+    }
+}
+
+class Calculation {
+    public static int multiplyingDigits() {
         int number1 = 10;
         int number2 = 15;
         int mul = number1 * number2;
-        System.out.println("Multiplication of Two Number is::" + " " + mul);
+        return mul;
     }
 }
