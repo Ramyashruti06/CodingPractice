8201f668d45455aa29d4f54c2781341629262b32
CodeTalks <info@codetalksdna.com>
2023-04-20 10:34:54 +0530

    Fromatted code as Object

--- src/codetalksdna/arithmeticOperations/AdditionOfNumbers.java
+++ src/codetalksdna/arithmeticOperations/AdditionOfNumbers.java
@@ -2,9 +2,17 @@ package codetalksdna.arithmeticOperations;
 
 public class AdditionOfNumbers {
     public static void main(String[] args) {
+        Number number = new Number();
+        int sum = number.sumOfNumber();
+        System.out.println("The Sum of Two Digit is::" + sum);
+    }
+}
+
+class Number {
+    public int sumOfNumber() {
         int number1 = 10;
         int number2 = 20;
         int sum = number1 + number2;
-        System.out.println("The Sum of Two Digit is::"+ sum);
+        return sum;
     }
 }
--- src/codetalksdna/arithmeticOperations/Simplification.java
+++ src/codetalksdna/arithmeticOperations/Simplification.java
@@ -3,26 +3,26 @@ package codetalksdna.arithmeticOperations;
 public class Simplification {
     public static void main(String[] args) {
         Equation equation = new Equation();
-
-
-        class Equation {
-            public static int simplifyingequation() {
-                int num1 = 80;
-                int num2 = 3;
-                int num3 = 13;
-                int num4 = 15;
-                int num5 = 8;
-                int num6 = 2;
-                int num7 = 7;
-                int sub1 = num4 - num5;
-                int mul1 = num6 * num7;
-                int mul2 = num2 * num3;
-                int sub2 = num1 - mul2;
-                int total1 = num1 - num2 * num3 + (num4 - num5) - num6 * num7;
-                int total2 = sub2 + sub1 - mul1;
-            }
-        }
-
-        System.out.println("Simplification of the given equation in one Way:" + "" + total1 + " " + "In Another way is " + "" + total2);
+        int result = equation.simplifyingequation();
+        System.out.println("The result is::" + "" + result);
     }
 }
+
+class Equation {
+    public static int simplifyingequation() {
+        int num1 = 80;
+        int num2 = 3;
+        int num3 = 13;
+        int num4 = 15;
+        int num5 = 8;
+        int num6 = 2;
+        int num7 = 7;
+        int sub1 = num4 - num5;
+        int mul1 = num6 * num7;
+        int mul2 = num2 * num3;
+        int sub2 = num1 - mul2;
+        int total1 = num1 - num2 * num3 + (num4 - num5) - num6 * num7;
+        return total1;
+    }
+}
+
