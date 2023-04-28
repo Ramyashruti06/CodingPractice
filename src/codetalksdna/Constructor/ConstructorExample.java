d7269e136d271d1c57b2fe2956f82ad3b25d7f4e
CodeTalks <info@codetalksdna.com>
2023-04-28 11:21:42 +0530

    Added concept of method overriding and overloading.

---
+++ src/codetalksdna/Constructor/ConstructorExample.java
@@ -0,0 +1,8 @@
+package codetalksdna.Constructor;
+
+public class ConstructorExample {
+    public static void main(String[] args) {
+        //16Bytes of Method
+        Student student = new Student();
+    }
+}
---
+++ src/codetalksdna/Constructor/Student.java
@@ -0,0 +1,5 @@
+package codetalksdna.Constructor;
+
+public class Student {
+
+}
