8893872344d575e65637e0c0a0d0b6676edd14e7
CodeTalks <info@codetalksdna.com>
2023-05-02 11:23:48 +0530

    Added Programs of Arrays.

---
+++ src/codetalksdna/Arrays/ArraysOutOfBound.java
@@ -0,0 +1,13 @@
+package codetalksdna.Arrays;
+
+import java.util.Arrays;
+
+public class ArraysOutOfBound {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        numbers[0] = 20;
+        numbers[1] = 30;
+        numbers[2] = 23;
+        System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ src/codetalksdna/Arrays/SimpleArrayExample.java
@@ -0,0 +1,8 @@
+package codetalksdna.Arrays;
+
+public class SimpleArrayExample{
+    public static void main(String[] args) {
+        int[] emp_id = {1,2,3,4,5,6};
+        System.out.println(emp_id[2]);
+    }
+}
