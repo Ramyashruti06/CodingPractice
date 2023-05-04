a5ead2701d713cb4cb32e9b14d69435650499353
CodeTalks <info@codetalksdna.com>
2023-05-04 11:42:28 +0530

    Added PrimeNumber.

--- src/codetalksdna/CodingProblems/Fibonacci.java
+++ src/codetalksdna/CodingProblems/Fibonacci.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a value till which you require to print the series: ");
+        System.out.println("Enter a value till which you require to print the series::?");
         int n = sc.nextInt();
         int firstTerm, secondTerm = 0, nextTerm = 1;
         for (int i = 1; i <= n; i++) {
