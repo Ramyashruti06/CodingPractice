fe644ebe304286e4118e5e942e3f89afdcbdb370
CodeTalks <info@codetalksdna.com>
2023-05-04 10:28:16 +0530

    Fixed Issue with Quadratic Equation.

--- src/codetalksdna/CodingProblems/RootsOfQuadraticEquation.java
+++ src/codetalksdna/CodingProblems/RootsOfQuadraticEquation.java
@@ -1,21 +1,28 @@
 package codetalksdna.CodingProblems;
 
 import java.util.Scanner;
+
 public class RootsOfQuadraticEquation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the values of a, b, c" /*+ a + b + c*/);
-        double a = sc.nextInt();
-        double b = sc.nextInt();
-        double c = sc.nextInt();
-        double root1, root2;
+        System.out.println("Enter the values of a");
+        double a = sc.nextDouble();
+        System.out.println("Enter the values of b");
+        double b = sc.nextDouble();
+        System.out.println("Enter the values of c");
+        double c = sc.nextDouble();
 
-        double det = b * b * 4 * a * c;
+        double root1 = 0, root2 = 0;
+
+        double det = b * b - 4 * a * c;
+        System.out.println("Determinant of the Following Equation is ::" + " " + det);
         if (det > 0) {
-            root1 = (- b + Math.sqrt(det)) / (2 * a);
-            root2 = (- b - Math.sqrt(det)) / (2 * a);
-            System.out.println("root1 = %.2f and root2 = %.2f"+ root1 + root2);
+            root1 = (-b + Math.sqrt(det)) / (2 * a);
+            root2 = (-b - Math.sqrt(det)) / (2 * a);
+            System.out.println("Roots of Quadratic Equations are" + " " + root1 + " , " + root2);
         }
-
+        if (det == 0) {
+            System.out.println("Roots of Quadratic Equations are Equal" + " " + root1 + " , " + root2);
+        }
     }
 }
