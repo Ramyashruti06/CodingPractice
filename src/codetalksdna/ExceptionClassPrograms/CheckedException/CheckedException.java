9bc719d3b7b30ff385a0cc9e773fc32b786f9553
CodeTalks <info@codetalksdna.com>
2023-05-09 10:36:59 +0530

    Finally Keyword Used

--- src/codetalksdna/ExceptionClassPrograms/CheckedException/CheckedException.java
+++ src/codetalksdna/ExceptionClassPrograms/CheckedException/CheckedException.java
@@ -3,15 +3,19 @@ package codetalksdna.ExceptionClassPrograms.CheckedException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CheckedException {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = null;
         try {
             File file = new File("C:\\Users\\nbhushan\\Downloads\\Shivam.pdf");
-            FileInputStream fis = new FileInputStream(file);
+            fis = new FileInputStream(file);
             System.out.println("Hey ! I Got the file !!");
         } catch (FileNotFoundException e) {
             System.out.println(e);
+        } finally {
+            fis.close();
         }
     }
 }
