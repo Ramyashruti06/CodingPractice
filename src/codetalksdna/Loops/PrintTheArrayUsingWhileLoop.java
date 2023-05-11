01fdd6404d82702828638e97a4b93a0296f5a640
Ramyashruti06 <kramyashruti06@gmail.com>
2023-05-11 10:38:11 +0530

    Array programs

--- src/codetalksdna/Loops/PrintTheArrayUsingWhileLoop.java
+++ src/codetalksdna/Loops/PrintTheArrayUsingWhileLoop.java
@@ -1,7 +1,21 @@
 package codetalksdna.Loops;
 
+import java.util.Scanner;
 public class PrintTheArrayUsingWhileLoop {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the values of array : ");
+        int[] n = new int[6];
+        int index = 0;
+        n[0] = sc.nextInt();
+        n[1] = sc.nextInt();
+        n[2] = sc.nextInt();
+        n[3] = sc.nextInt();
+        n[4] = sc.nextInt();
+        n[5] = sc.nextInt();
+        while(index <= n.length) {
+            index++;
+        }
+        System.out.println("The array is : " + n);
     }
 }
--- src/codetalksdna/Loops/PrintTheMultiplicationUsingWhileLoop.java
+++ src/codetalksdna/Loops/PrintTheMultiplicationUsingWhileLoop.java
@@ -1,7 +1,17 @@
 package codetalksdna.Loops;
 
+import java.util.Scanner;
 public class PrintTheMultiplicationUsingWhileLoop {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int i =  1;
+        System.out.println("The multiplication of " + n + " is :");
+        while(i <= 10) {
+            System.out.println(n + " * " + i + " = " + (n * i));
+            i++;
+        }
 
     }
 }
