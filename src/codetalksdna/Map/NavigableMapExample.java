e18e71da33d9dc5e68be9ae8da0da7af643c9a01
codetalks95 <info@codetalksdna.com>
2023-06-12 10:46:57 +0530

    Added new Package for Queue and Map.

---
+++ src/codetalksdna/Map/NavigableMapExample.java
@@ -0,0 +1,19 @@
+package codetalksdna.Map;
+
+import java.util.NavigableMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class NavigableMapExample {
+    public static void main(String[] args) {
+        NavigableMap<String, Integer> stringIntegerMap = new TreeMap<>();
+        stringIntegerMap.put("Shruti", 1);
+        stringIntegerMap.put("Nitesh", 2);
+        stringIntegerMap.put("Apple", 3);
+        stringIntegerMap.put("Kumar", 4);
+        System.out.println(stringIntegerMap.firstEntry());
+        System.out.println(stringIntegerMap.lastEntry());
+        System.out.println(stringIntegerMap.descendingMap());
+        System.out.println(stringIntegerMap);
+    }
+}
