057cd498f28261d149368b335e973034536717f4
codetalks95 <info@codetalksdna.com>
2023-06-15 11:33:34 +0530

    Path code added.

--- src/codetalksdna/PathOfProgram/PathOfProgram.java
+++ src/codetalksdna/PathOfProgram/PathOfProgram.java
@@ -1,14 +1,10 @@
 package codetalksdna.PathOfProgram;
 
 public class PathOfProgram {
-    String path1 = "D:\\Intellij-Idea-Projects\\CodingPractice\\";
-    String path2 = "C:\\Users\\kramy\\IdeaProjects\\CodingPractice";
-
-    //String path = "";
+    String path = "D:\\Intellij-Idea-Projects\\CodingPractice\\";
+    //String path = "C:\\Users\\kramy\\IdeaProjects\\CodingPractice";
+    
     public String getPath() {
-        return path1;
-
-
-
+        return path;
     }
 }
