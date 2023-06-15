4b1df2ca6dd3530a80ad2c316c68061714577d7d
codetalks95 <info@codetalksdna.com>
2023-06-15 11:03:10 +0530

    Added new Code in Serialization.

--- src/codetalksdna/serialization/SerializationUID.java
+++ src/codetalksdna/serialization/SerializationUID.java
@@ -1,7 +1,23 @@
 package codetalksdna.serialization;
 
-public class SerializationUID {
-    public static void main(String[] args) {
+import java.io.*;
 
+class SerializationUIDExample implements Serializable {
+    String userName = "Nitesh";
+    String password = "Bhushan";
+}
+
+public class SerializationUID {
+    private static final long serialVersionUID = 4L;
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        SerializationUIDExample serializationTransientKeyword = new SerializationUIDExample();
+        FileOutputStream fileOutputStream = new FileOutputStream("abc.ser");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(serializationTransientKeyword);
+
+        FileInputStream fileInputStream = new FileInputStream("abc.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        codetalksdna.serialization.SerializationTransientKeyword serializationTransientKeyword1 = (codetalksdna.serialization.SerializationTransientKeyword) objectInputStream.readObject();
+        System.out.println(serializationTransientKeyword1.userName + " " + serializationTransientKeyword1.password);
     }
 }
