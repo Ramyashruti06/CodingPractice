1d0dd65f1e7afaeb69b5d3b5f74faf8547e49ca1
Ramyashruti06 <kramyashruti06@gmail.com>
2023-06-17 09:50:10 +0530

    Added new programs

--- src/codetalksdna/PathOfProgram/PathOfProgram.java
+++ src/codetalksdna/PathOfProgram/PathOfProgram.java
@@ -2,7 +2,7 @@ package codetalksdna.PathOfProgram;
 
 public class PathOfProgram {
     String path1 = "D:\\Intellij-Idea-Projects\\CodingPractice\\";
-    String path2 = "C:\\Users\\kramy\\IdeaProjects\\CodingPractice";
+    String path2 = "C:\\Users\\kramy\\IdeaProjects\\CodingPractice\\";
 
     //String path = "";
     public String getPath() {
