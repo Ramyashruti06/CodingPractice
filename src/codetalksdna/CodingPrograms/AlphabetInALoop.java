febc2d95a515abda19c23d23de7e113fec7c2ba2
codetalks95 <info@codetalksdna.com>
2023-07-12 08:18:41 +0530

    added simple print statement

--- src/codetalksdna/CodingPrograms/AlphabetInALoop.java
+++ src/codetalksdna/CodingPrograms/AlphabetInALoop.java
@@ -2,6 +2,7 @@ package codetalksdna.CodingPrograms;
 
 public class AlphabetInALoop {
     public static void main(String[] args) {
+        System.out.println("The Alphabets in the English Dictionary is :");
         char alphabet;
         for (alphabet = 'A'; alphabet <= 'Z'; ++alphabet) {
             System.out.print(alphabet + " ");
